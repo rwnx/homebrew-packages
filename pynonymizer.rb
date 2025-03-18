ef0f68b9263feb4e3ca68837af2aca571da5d0a1
Rowan Twell <rowantwell@gmail.com>
2025-03-18 14:34:55 +0000

    update to latest

--- pynonymizer.rb
+++ pynonymizer.rb
@@ -3,18 +3,20 @@ class Pynonymizer < Formula
 
   desc "Tool for anonymizing data in databases"
   homepage "https://github.com/username/pynonymizer"
-  url "https://files.pythonhosted.org/packages/source/p/pynonymizer/pynonymizer-1.0.0.tar.gz"
-  sha256 "23664ec74f87edb59be51ecb6cb915ea889dcd214601d6fa53a8c27b94937513"
+  url "https://files.pythonhosted.org/packages/source/p/pynonymizer/pynonymizer-2.5.0.tar.gz"
+  sha256 "fa6a68a4c3f898ee15446aeb86948bf7bbe27f5987b314e9b41de37cd5bbd519"
   license "MIT"
 
   livecheck do
     url "https://pypi.org/simple/pynonymizer/"
-    regex(%r{href=.*?/pynonymizer-(\d+(?:\.\d+)*)\.tar\.gz}i)
+    regex(%r{href=.*?/pynonymizer-(\d+(?:\.\d+)*)\.t}i)
   end
 
+  depends_on "libyaml"
   depends_on "python3"
   depends_on "mysql-client" => :optional
-  depends_on "postgresql" => :optional
+  depends_on "postgresql@14" => :optional
+
 
   def install
     # Use the recommended virtualenv_install_with_resources instead
