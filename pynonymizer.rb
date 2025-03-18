75caa288f6fee55cac446684731a72267f0fac73
Rowan Twell <rowantwell@gmail.com>
2025-03-18 14:39:11 +0000

    lint

--- pynonymizer.rb
+++ pynonymizer.rb
@@ -2,6 +2,7 @@ class Pynonymizer < Formula
   include Language::Python::Virtualenv
 
   desc "Tool for anonymizing data in databases"
+  source "https://pypi.org/project/pynonymizer/#files"
   homepage "https://github.com/username/pynonymizer"
   url "https://files.pythonhosted.org/packages/source/p/pynonymizer/pynonymizer-2.5.0.tar.gz"
   sha256 "fa6a68a4c3f898ee15446aeb86948bf7bbe27f5987b314e9b41de37cd5bbd519"
@@ -16,7 +17,6 @@ class Pynonymizer < Formula
   depends_on "python3"
   depends_on "mysql-client" => :optional
   depends_on "postgresql@14" => :optional
-
 
   def install
     # Use the recommended virtualenv_install_with_resources instead
